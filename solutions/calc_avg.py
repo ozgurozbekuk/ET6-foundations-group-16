0408f6e6c07f6c6b70a4021a538ea7dd2f7bd2e9
Bikaze <clmntmugisha@gmail.com>
2025-01-04 13:09:16 +0200

    Write function level documentation

--- solutions/calc_avg.py
+++ solutions/calc_avg.py
@@ -1,4 +1,10 @@
 def calculate_average(numbers):
+    """
+    Calculates the average of a list of numbers (integers or floats).
+    Returns:
+    - float: Average of the numbers.
+    - None: If the list is empty.
+    """
     if not numbers:
         return None
     return sum(numbers) / len(numbers)
