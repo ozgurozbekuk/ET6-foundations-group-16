8ce11c25295f07b86bb0ee11743f719ea64f669b
Bikaze <clmntmugisha@gmail.com>
2025-01-04 13:24:59 +0200

    Test calc_avg.py on some standard cases

--- solutions/tests/test_calc_avg.py
+++ solutions/tests/test_calc_avg.py
@@ -16,4 +16,15 @@ from solutions.calc_avg import calculate_average
 class TestCalculateAverage(unittest.TestCase):
     """Test suite for the calculate_average function."""
 
-    pass
+    # Standard test cases
+    def test_integers(self):
+        """It should return the average of multiple integers."""
+        self.assertEqual(calculate_average([10, 20, 30, 40, 50]), 30.0)
+
+    def test_floats(self):
+        """It should return the average of multiple floats."""
+        self.assertAlmostEqual(calculate_average([1.1, 2.2, 3.3]), 2.2, places=1)
+
+
+if __name__ == "__main__":
+    unittest.main()
