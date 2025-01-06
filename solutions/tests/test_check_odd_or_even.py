d56f94cad67256ac630e863f34a0be09839f4b9f
Bikaze <clmntmugisha@gmail.com>
2025-01-06 13:16:06 +0200

    Add class level docstrings

--- solutions/tests/test_check_odd_or_even.py
+++ solutions/tests/test_check_odd_or_even.py
@@ -3,7 +3,7 @@ from solutions.calculate_average import check_odd_or_even
 
 
 class TestCheckOddOrEven(unittest.TestCase):
-    pass
+    """Test suite for the check_odd_or_even function."""
 
 
 if __name__ == "__main__":
