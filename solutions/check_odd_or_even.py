d3599974c569fb3b25b3b4db41a5bebcd4e733b6
Bikaze <clmntmugisha@gmail.com>
2025-01-06 13:01:13 +0200

    Added function level docstrings

--- solutions/check_odd_or_even.py
+++ solutions/check_odd_or_even.py
@@ -1,3 +1,14 @@
 def check_odd_or_even(num):
+    """Checks if the provided integer is even or odd.
+
+    Parameters:
+        num (int): The integer to check.
+
+    Returns:
+        str: "Even" if num is even, otherwise "Odd".
+
+    Raises:
+        AssertionError: If num is not an integer.
+    """
     assert isinstance(num, int), "num must be an integer"
     return "Even" if num % 2 == 0 else "Odd"
